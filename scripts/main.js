5c096edad6e71750958769514ab8fe8b45939648
xiangan wang <wang.xianga@northeastern.edu>
2024-01-23 10:48:05 -0500

    2nd commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,12 @@
+// Access the first <h1> element and set its text content
 const webHeading = document.querySelector("h1");
 webHeading.textContent = "Welcome to my webpage!";
 
+// Get elements for image switching functionality
 const switchButton = document.getElementById("switchButton");
 const webImage = document.getElementById("webImage");
 
+// Define onlick event for switchButton to toggle the image source
 switchButton.onclick = () => {
   const mySrc = webImage.getAttribute("src");
   if (mySrc === "images/img1.jpeg") {
@@ -13,9 +16,11 @@ switchButton.onclick = () => {
   }
 };
 
+// User name setting functionality
 let myButton = document.getElementById("setNameButton");
 let myHeading = document.querySelector("h1");
 
+// Function to prompt for user's name and store it in localStorage
 function setUserName() {
     const myName = prompt("Please enter your name.");
     if (!myName) {
@@ -26,6 +31,7 @@ function setUserName() {
     }
   };
 
+  // Initialize webpage with stored name or prompt for a new name
   if (!localStorage.getItem("name")) {
     setUserName();
   } else {
@@ -33,10 +39,12 @@ function setUserName() {
     myHeading.textContent = `Welcome to my webpage, ${storedName}!`;
   };
   
+  // Set onclick event for myButton to change the user's name
   myButton.onclick = () => {
     setUserName();
   };
-  
+
+  // Add an additional click events listener to setNameButton for an alert
   document.getElementById("setNameButton").addEventListener("click", function() {
     alert("Your username has been changed!")
   }); 
